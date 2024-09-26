028f0f1d37e58a0bc6a216f8a9ffba18efc44ad0
hanwoosung <yiok79@naver.com>
2024-09-26 17:33:21 +0900

    한글 꺠짐 수정

--- src/test12/database/DBConn.java
+++ src/test12/database/DBConn.java
@@ -18,6 +18,7 @@ public class DBConn {
 
     private DBConn() {
         try {
+
             Properties properties = new Properties();
             FileInputStream input = new FileInputStream("src/db.properties");
             properties.load(input);
